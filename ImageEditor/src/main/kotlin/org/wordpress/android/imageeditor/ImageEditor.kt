f346a3d449aa487d4f4afd29db024e6f7ce87017
ashiagr <agr.ash@gmail.com>
2020-01-23 14:51:08 +0530

    Make ImageEditor singleton

--- ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/ImageEditor.kt
+++ ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/ImageEditor.kt
@@ -10,8 +10,11 @@ import com.yalantis.ucrop.UCrop.Options
 import com.yalantis.ucrop.model.AspectRatio
 import com.yalantis.ucrop.view.CropImageView
 import java.io.File
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class ImageEditor {
+@Singleton
+class ImageEditor @Inject constructor() {
     /**
      * @param context self explanatory
      * @param contentUri URI of initial media - can be local or remote

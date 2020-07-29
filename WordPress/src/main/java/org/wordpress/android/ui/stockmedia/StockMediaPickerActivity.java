89dc858b2dbde4e9ecb4777625158afa2a6fbcd5
Vojta Smrcek <vojta.smrcek@automattic.com>
2020-07-29 17:06:27 +0200

    Revert "Issue/12324 storage limit with stock media"

--- WordPress/src/main/java/org/wordpress/android/ui/stockmedia/StockMediaPickerActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/stockmedia/StockMediaPickerActivity.java
@@ -187,10 +187,6 @@ public class StockMediaPickerActivity extends LocaleAwareActivity implements Sea
 
         mTextAdd.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
-                if (null != mSite && mSite.hasDiskSpaceQuotaInformation() && mSite.getSpaceAvailable() <= 0) {
-                    ToastUtils.showToast(StockMediaPickerActivity.this, R.string.error_media_quota_exceeded_toast);
-                    return;
-                }
                 uploadSelection();
             }
         });
